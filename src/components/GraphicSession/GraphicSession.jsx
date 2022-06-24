bc014187d312dad617bd0734aa89df962e08e098
Etha <76168237+etharion29@users.noreply.github.com>
2022-06-24 16:53:18 +0200

    Little color modification axisX

--- src/components/GraphicSession/GraphicSession.jsx
+++ src/components/GraphicSession/GraphicSession.jsx
@@ -13,7 +13,7 @@ const GraphicSession = () => {
             <label className="graphic_session_title">Durée moyenne des sessions</label>
             <LineChart width={200} height={150} data={data?.data.sessions}>
                 <CartesianGrid vertical="" horizontal="" />
-                <XAxis dataKey="day"  tickLine="" axisLine="" />
+                <XAxis dataKey="day"  tickLine="" axisLine="" stroke="rgba(255, 255, 255, 0.681)" />
                 <YAxis hide="true" tickLine="" axisLine="" />
                 <Tooltip />
                 <Line type="monotone" dataKey="sessionLength" stroke="#FFFFFF" />
