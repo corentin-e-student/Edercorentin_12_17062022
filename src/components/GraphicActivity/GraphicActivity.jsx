f00a5b2f3acf789198c9944a224bc897cd4307d8
Etha <76168237+etharion29@users.noreply.github.com>
2022-06-26 15:36:57 +0200

    Modification responsivity graphicActivity

--- src/components/GraphicActivity/GraphicActivity.jsx
+++ src/components/GraphicActivity/GraphicActivity.jsx
@@ -1,7 +1,19 @@
 import useUserActivity from "../../hooks/useUserActivity";
 import "./graphic_activity.css"
 import React from "react";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from "recharts";
+
+const CustomTooltip = ({ payload, active }) => {
+    if (active) {
+     return (
+       <div className="graphic_activity_custom_tooltip">
+         <label className="graphic_activity_custom_value_kilogram">{payload[0].value} kg</label>
+         <label className="graphic_activity_custom_value_calories">{payload[1].value} Kcal</label>
+       </div>
+     )
+   }
+   return null
+}
 
 const GraphicActivity = () => {
 
@@ -15,7 +27,7 @@ const GraphicActivity = () => {
                 <CartesianGrid strokeDasharray="4" vertical=""/>
                 <XAxis dataKey="session" tickLine=""/>
                 <YAxis tickLine="" axisLine="" orientation="right"/>
-                <Tooltip />
+                <Tooltip content={<CustomTooltip/>}/>
                 <Legend verticalAlign="top" align="right" height= {80} />
                 <Bar radius={[10, 10, 0, 0]} barSize={10} dataKey="kilogram" fill="#282D30" unit=" kg" />
                 <Bar radius={[10, 10, 0, 0]} barSize={10} dataKey="calories" fill="#E60000" unit=" kCal" />
