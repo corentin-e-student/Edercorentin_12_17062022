e8e4a60285b2b9c2f3144cdd8a64acb936249e5b
Etha <76168237+etharion29@users.noreply.github.com>
2022-07-01 08:56:45 +0200

    Fix bug graphic

--- src/components/GraphicObjectif/GraphicObjectif.jsx
+++ src/components/GraphicObjectif/GraphicObjectif.jsx
@@ -1,7 +1,7 @@
 import useUser from "../../hooks/useUser";
 import "./graphic_objectif.css"
 import React from "react";
-import { RadialBarChart, RadialBar, ResponsiveContainer} from 'recharts';
+import { RadialBarChart, RadialBar} from 'recharts';
 
 const GraphicObjectif = () => {
 
@@ -9,23 +9,20 @@ const GraphicObjectif = () => {
 
     return (
         <div className="graphic_objectif_background">
-            <label className="graphic_objectif_title">Score</label>
-            <ResponsiveContainer width='99%' height='100%'>
-                <RadialBarChart
-                    innerRadius={75}
-                    outerRadius={85}
-                    startAngle={80}
-                    endAngle={360 + 80}
-                    barSize={10}
-                    data={data?.data.userInfos}
-                    >
-                    <RadialBar
-                        cornerRadius={10}
-                        fill="#ff0000"
-                        dataKey="score"
-                    />
-                </RadialBarChart>
-            </ResponsiveContainer>
+            <label className="graphic_objectif_title">Score {data.data.userInfos.score * 100} %</label>
+            <RadialBarChart
+                cx="50%" 
+                cy="50%"
+                innerRadius={75}
+                barSize={10}
+                data={data?.data.userInfos}
+                >
+                <RadialBar
+                    cornerRadius={10}
+                    fill="#ff0000"
+                    dataKey="score"
+                />
+            </RadialBarChart>
         </div>
     );
 }
